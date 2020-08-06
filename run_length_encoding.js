952173ad37d55c2a70d2ed2f3df9631f1bbd7bfa
Kyle McCurley <kmccurley123@gmail.com>
2020-08-06 01:38:48 -0400

    Update run_length_encoding.js

--- run_length_encoding.js
+++ run_length_encoding.js
@@ -1,1 +1,28 @@
+function encode(str) {
+  let current_char = str[0]; // a
+  let count = 0;
+  let encodedString = '';
+  for(let i = 0; i < str.length; i++) {
+    var char = str[i]; // a
+    if (current_char === char) {
+      count++;
+    } else if (current_char !== char) {
+      if (count === 1) {
+        encodedString += current_char
+      } else {
+        encodedString += current_char;
+        encodedString += count;
+      }
 
+      count = 1;
+      current_char = char;
+    }
+  }
+  // If there is a leftover character
+  if (char) {
+    encodedString += char;
+    encodedString += count;
+  }
+
+  return encodedString;
+}
